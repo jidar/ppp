5ebe5c5298d29a582f3480a2daac14265dba09f8
xenos <xenos@MonolithMobile.(none)>
2012-09-23 20:47:57 -0500

    Added new delete and list entries.  Added add- list- and remove- projects.  Added remote-days.

--- engine.py
+++ engine.py
@@ -1,64 +1,217 @@
 import ConfigParser
+import pickle
+import os
+import sys
+import time
+import datetime
+from uuid import uuid4
+
 PROBLEM='problem'
 PROGRESS='progress'
 PLAN='plan'
-
-class Entry(object):
-    def __init__(self):
-        self.timestamp = None
-        self.project = None
-        self.classification = None
-        self.text = None        
+PPPTYPES = (PROBLEM, PROGRESS, PLAN)
 
 #Datafile Management
 def load_datafile(datafile_path):
-    pass
+    datafile = None
+    fh = None
+    
+    try:
+        datafile_path = os.path.expanduser(datafile_path)
+    except Exception as e:
+        sys.stderr.write(str(e))
+        sys.stderr.write('Error expanding path to datafile for load')
+        sys.stderr.flush()
 
-def save_datafile(datafile_path):
-    pass
+    try:
+        if not os.path.exists(datafile_path):
+            os.mkdir(datafile_path)
+    except Exception as e:
+        sys.stderr.write(str(e))
+        sys.stderr.write('Error verifying existance fo datafile path for load')
+        sys.stderr.flush()
+
+    datafile_path = os.path.join(datafile_path, 'datafile')
+
+    try:
+        fh = open(datafile_path, 'r')
+    except:
+        sys.stderr.write('No datafile found, starting from scratch')
+    
+    if fh is not None:
+        try:
+            datafile = pickle.load(fh)
+        except Exception as e:
+            sys.stderr.write(str(e))
+            sys.stderr.write('Error unpickling datafile')
+
+    if datafile is None:
+        #Initialize datafile if this is the first run
+        datafile = DataFile()
+
+    if fh is not None:
+        fh.close()
+
+    return datafile
+
+def save_datafile(datafile, datafile_path):
+    fh = None
+
+    try:
+        datafile_path = os.path.expanduser(datafile_path)
+    except Exception as e:
+        sys.stderr.write(str(e))
+        sys.stderr.write('Error expanding path to datafile for dump')
+        sys.stderr.flush()
+    
+    datafile_path = os.path.join(datafile_path, 'datafile')
         
+    try:
+        fh = open(datafile_path, 'w+')
+    except Exception as e:
+        sys.stderr.write(str(e))
+        sys.stderr.write('Error opening datafile for dump: %s' % str(datafile_path))        
+
+    if fh is not None:
+        try:
+            pickle.dump(datafile, fh)
+        except Exception as e:
+            sys.stderr.write(str(e))
+            sys.stderr.write('Error dumping datafile')
+        
+        fh.close()
+
+class Entry(object):
+    def __init__(self, project_name, classification, text):
+        self.id = uuid4()
+        self.timestamp = time.time()
+        self.datetimestamp = datetime.datetime.now()
+        self.project = project_name
+        self.classification = classification
+        self.text = text
+        
+    def __repr__(self):
+        s = ''
+        s += 'id: %s\n' % (str(self.id))
+        s += 'date: %s\n' % (str(self.datetimestamp))
+        s += 'project: %s\n' % (str(self.project))
+        s += 'type: %s\n' % (str(self.classification))
+        s += 'text: %s\n' % (str(self.text))
+        return s
+        
+class Project(object):
+    def __init__(self, name, display_name):
+        self.name = name
+        self.display_name = display_name
+
 class DataFile(object):
     def __init__(self):
         self._entries = []
         self._project_names = []
 
-    #Entry Management
-    def append_entry(entry_object):
+    #Entry Management        
+    def print_entries(self):
+        if self._entries == []:
+            print 'No entries listed.'
+        for e in self._entries:
+            print str(e)
+
+    def append_entry(self, entry_object):
         self._entries.append(entry_object)
-    
-    def get_project_names():
-        return self._project_names
+        
+    def delete_entry(self, entry_id):
+        for e in self._entries:
+            if str(e.id).lower() == str(entry_id).lower():
+                self._entries.remove(e)
+                return True
     
     #Project Name Management
-    def add_project_name(new_project_name):
-        if str(new_project_name).lower() not in [str(pn).lower() for pn in self._project_names]:
-            self._project_names.append(new_project_name)
+    def get_all_project_names(self):
+        '''returns a lis of tuples'''
+        return [(p.name, p.display_name) for p in self._project_names]
+    
+    def get_project_names(self):
+        return [p.name for p in self._project_names]
+    
+    def add_project_name(self, name, display_name):
+        if str(name).lower() not in [str(pn.name).lower() for pn in self._project_names]:
+            self._project_names.append(Project(name, display_name))
             return True
         else:
             return False
     
-    def delete_project_name(project_name):
-        if str(project_name).lower() not in [str(e.project).lower() for e in self._entries]:
-            self._project_names.remove(project_name)
-            return True
+    def delete_project_name(self, project_name):
+        #offending_p = None
+        for p in self._project_names:
+            if str(project_name).lower() == str(p.name).lower():
+                self._project_names.remove(p)
+                return True
         else:
             return False
+        
+    def _report(self, entries, projects=None):
+        #Make a list of the projects to include in the report
+        req_projects = []
+        if projects is not None:
+            for req_pname in projects:
+                for p in self._project_names:                
+                    if str(req_pname).lower() == (p.name).lower():
+                        req_projects.append(p)
+        else:
+            req_projects = self._project_names
 
-#    
-#def archive_entry():
-#    #Append entry to archive entry list
-#    #remove entry from entries list
-#    pass
-#
-#def 
-#
-#def save_report():
-#    pass
-#
-#def open_report():
-#    pass
-#
-#def est():
-#    scp = ConfigParser.SafeConfigParser()
-#    
-#    scp.add_section()
+        #Create the report on a per-project, per-type basis
+        report_dict = {}
+    
+        for p in req_projects:
+            pdict = p.display_name
+            pname = p.name
+            report_dict[pdict] = {}
+            for t in PPPTYPES:
+                ttype = t
+                report_dict[pdict][ttype] = []
+                for e in entries:
+                    ename = e.project
+                    etype = e.classification
+                    etext = e.text
+                    if (ename == pname) and (etype == ttype):
+                            report_dict[pdict][ttype].append(etext)
+
+        for p in report_dict:
+            if report_dict[p] != {} or report_dict[p] != None:
+                print p
+            else:
+                print 'Empty: %s' % str(e)
+            for t in report_dict[p]:
+                if report_dict[p][t] != []:
+                    print '    ' + t
+                for e in report_dict[p][t]:
+                    print '        ' + e + '\n'
+
+    #note: then < now
+    def report_days(self, days, projects=None):
+        then = datetime.datetime.now() - datetime.timedelta(days=days)
+        then = datetime.datetime(then.year, then.month, then.day)
+        requested_entries = []
+        for e in self._entries:
+            if e.datetimestamp > then:
+                requested_entries.append(e)
+
+        self._report(requested_entries, projects)
+        
+    def report_range(self, sy, sm, sd, ey=None, em=None, ed=None):
+        start = datetime.datetime(sy, sm, sd)
+        end = None
+        requested_entries = []
+        if ey and em and ed:
+            try:
+                end = datetime.datetime(ey, em, ed)
+            except Exception as e:
+                sys.stderr.write(str(e))
+                sys.stderr.write('Improper date format given to range reporter')
+
+        for e in self._entries:
+            if e.datetimestamp > start and e.datetimestamp < end:
+                requested_entries.append(e)
+        
+        self._report(requested_entries, projects)                
--- ppp.py
+++ ppp.py
@@ -1,39 +1,114 @@
 import argparse
 import engine
 
+DATAFILE_PATH='~/.ppp'
+
 class MyAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         print "parser: %s" % str(parser)
         print "namespace: %s" % str(namespace)
         print "values: %s" % str(values)
         print "option_string: %s" % str(option_string)
+
+def run(args, datafile):
     
-    pass
+    #Entry Management
+    if args.command == 'new':
+        entry = engine.Entry(args.project, args.ppp_type, args.text)
+        datafile.append_entry(entry)
+    
+    if args.command == 'list':
+        datafile.print_entries()
+
+    if args.command == 'delete':
+        r = datafile.delete_entry(args.entry_id)
+        if r:
+            print 'Entry %s deleted successfully' % str(args.entry_id)
+        else:
+            print 'Entry %s not deleted (not found)' % str(args.entry_id)
+    
+    #Project Management
+    if args.command == 'add-project':
+        datafile.add_project_name(args.project_name, args.project_display_name)
+    
+    if args.command == 'list-projects':
+        info = datafile.get_all_project_names()
+        for n, dn in info:
+            print "%s (%s)" % (str(dn), str(n))
+            
+    if args.command == 'remove-project':
+        if datafile.delete_project_name(args.project):
+            print 'Project %s deleted.' % str(args.project)
+        else:
+            print 'There are entries currently using this project name.\
+                   Delete those entries before removing the project name'
+
+    if args.command == 'report-days':
+        datafile.report_days(int(args.days), args.project_list)
+        pass
+
+
+    #This should always be the last thing
+    engine.save_datafile(datafile, DATAFILE_PATH)
+        
 
 if __name__ == '__main__':
     desc = 'PPP Report aggregator'
+    
     #Load data file
-    #load project names from
-    projects = ('cloudfiles', 'blockstorage')
-    ppptypes=('problem','progress','plan')
+    datafile = engine.load_datafile(DATAFILE_PATH)
+    
+    #load data from datafile
+    projects = datafile.get_project_names()
+    ppptypes = engine.PPPTYPES
+    if projects == []:
+        print 'No Projects Found.'
+        print 'Add a new project using: ppp add-project <project-name> <project-diplay-name>'
+    
+    #Argparsers
     parser = argparse.ArgumentParser(description=desc)
-    #pppgroup = parser.add_mutually_exclusive_group()
-    #pppgroup.add_argument('progress', help="Add progress report.", optional )
-    #pppgroup.add_argument('problem', help="Add progress report.")
-    #pppgroup.add_argument('plan', help="Add progress report.")
-    
-    parser.add_argument('--list', nargs=1, action='store', )
-        
-    subparsers = parser.add_subparsers(help='commands')
+    subparsers = parser.add_subparsers(help='commands', dest='command')
 
-    create_parser = subparsers.add_parser('add', help='Add a PPP entry')
-    create_parser.add_argument('project', action='store', choices=projects, help='Project tag for entry.')
-    create_parser.add_argument('ppp-type', action='store', choices=ppptypes, help='PPP Type tag for entry.')
-    create_parser.add_argument('text', action='store', help='PPP Type tag for entry.')
+    #Entry Management
+    new_entry_parser = subparsers.add_parser('new', help='Create new PPP entry')
+    new_entry_parser.add_argument('project', metavar='project-name', action='store', choices=projects, help='Project tag for entry.')
+    new_entry_parser.add_argument('ppp_type', metavar='ppp-type', action='store', choices=ppptypes, help='PPP Type tag for entry.')
+    new_entry_parser.add_argument('text', action='store', help='PPP Type tag for entry.')
+
+    del_entry_parser = subparsers.add_parser('delete', help='Delete old PPP entry')
+    del_entry_parser.add_argument('entry_id', metavar='entry-id', action='store', help='Project tag for entry.')
     
-    create_parser = subparsers.add_parser('report', help='Create a PPP report')
-    create_parser.add_argument('days', action='store', default=7, help='Days in past from today to report')
-    create_parser.add_argument('--projects', nargs='+', action='store', default='all', choices=projects, help='PPP Type tag for entry.')
+    list_entries_parser = subparsers.add_parser('list', help='List all entries')
+
+    #Project Name Management    
+    add_project_parser = subparsers.add_parser('add-project', help='Add new project name')
+    add_project_parser.add_argument('project_name', metavar='project-name', action='store', help='Name to reference project on command line')
+    add_project_parser.add_argument('project_display_name', metavar='project-display-name', action='store', help='Print name of project')
+
+    list_proj_parser = subparsers.add_parser('list-projects', help='List all added projects')
     
+    del_proj_parser = subparsers.add_parser('remove-project', help='Delete specific project name if no entries are currently using it')
+    del_proj_parser.add_argument('project', action='store', choices=projects, help='Project tag to delete.')        
+    
+    #Reporting
+    report_days_parser = subparsers.add_parser('report-days', help='Create a PPP report from X days ago until now')
+    report_days_parser.add_argument('days', action='store', default=7, help='Days in past from today to report')
+    report_days_parser.add_argument('--project-list', metavar='project_list', nargs='+', action='store', default=projects, choices=projects, help='PPP Type tag for entry.')
+
+    report_from_date_parser = subparsers.add_parser('report-from-date', help='Create a PPP report from a starting date until now')
+    report_from_date_parser.add_argument('start_year', action='store', help='Starting year for report range')
+    report_from_date_parser.add_argument('start_month', action='store', help='Starting year for report range')
+    report_from_date_parser.add_argument('start_day', action='store', help='Starting year for report range')
+    report_from_date_parser.add_argument('--project-list', metavar='project_list', nargs='+', action='store', default=projects, choices=projects, help='PPP Type tag for entry.')    
+
+    report_date_range_parser = subparsers.add_parser('report-date-range', help='Create a PPP report from a starting date to an ending date')
+    report_from_date_parser.add_argument('start_year', action='store', help='Starting year for report range')
+    report_from_date_parser.add_argument('start_month', action='store', help='Starting year for report range')
+    report_from_date_parser.add_argument('start_day', action='store', help='Starting year for report range')    
+    report_date_range_parser.add_argument('end_year', action='store', help='Ending year for report range')
+    report_date_range_parser.add_argument('end_month', action='store', help='Ending year for report range')
+    report_date_range_parser.add_argument('end_day', action='store', help='Ending year for report range')
+    report_date_range_parser.add_argument('--project-list', metavar='project_list', nargs='+', action='store', default=projects, choices=projects, help='PPP Type tag for entry.')    
+
     args = parser.parse_args()
-    print args
+    run(args, datafile)
